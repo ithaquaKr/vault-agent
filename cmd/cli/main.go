87347c3d7425e2b7e36a4cf386b664fdd28da5e9
Tuan Hiep <ithadev.nguyen@gmail.com>
2025-04-16 00:20:01 +0700

    feat(core): implement parse flag and define const flag config (#5)

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -8,14 +8,22 @@ import (
 	"syscall"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var Version = "dev"
 
+const cfgVaultAddress = "vault-addr"
+
+const cfgConfigPath = "config-path"
+
+var c = viper.New()
+
 var rootCmd = &cobra.Command{
-	Use:   "vault-agent-cli",
-	Short: "Sync configurations, secrets of Hashicorp Vault",
-	Long:  "This is a tool to help setup, management of Hashicorp Vault.",
+	Use:     "vault-agent-cli",
+	Short:   "Sync configurations, secrets of Hashicorp Vault",
+	Version: Version, // TODO: Implement versioning via another command
+	Long:    "This is a tool to help setup, management of Hashicorp Vault.",
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 	},
 }
@@ -36,7 +44,8 @@ func execute() {
 }
 
 func init() {
-	// TODO: Parse configurations
+	configVar(rootCmd, cfgVaultAddress, "http://127.0.0.1:8200", "The URL of the remote Vault", c)
+	configVar(rootCmd, cfgConfigPath, "", "The configs file path", c)
 }
 
 func main() {
